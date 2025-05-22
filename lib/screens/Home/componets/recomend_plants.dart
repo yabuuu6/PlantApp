2347f658961c8b2b43cae04a266cabf6f0e18e00
buyaramadhan <ytchochote@gmail.com>
2025-05-23 02:23:29 +0700

    daftar tanaman rekomendasi dengan navigasi ke halaman detail

---
+++ lib/screens/Home/componets/recomend_plants.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:plant_app/screens/details/details_screen.dart';
+import 'package:plant_app/constants.dart';
+
+class RecomendsPlants extends StatelessWidget {
+  const RecomendsPlants({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: [
+          RecomendPlantCard(
+            image: "assets/images/image_1.png",
+            title: "Samantha",
+            country: "Russia",
+            price: 440,
+            press: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const DetailsScreen()),
+              );
+            },
+          ),
+          RecomendPlantCard(
+            image: "assets/images/image_2.png",
+            title: "Angelica",
+            country: "Russia",
+            price: 440,
+            press: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const DetailsScreen()),
+              );
+            },
+          ),
+          RecomendPlantCard(
+            image: "assets/images/image_3.png",
+            title: "Samantha",
+            country: "Russia",
+            price: 440,
+            press: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
+
