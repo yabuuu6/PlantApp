183965b851cf14f4a3178a5c3e8389d5547570ed
buyaramadhan <ytchochote@gmail.com>
2025-05-23 02:18:48 +0700

    widget Body untuk menyusun tampilan halaman utama

---
+++ lib/screens/details/components/body.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:plant_app/constants.dart';
+
+import 'image_and_icons.dart';
+import 'title_and_price.dart';
+
+class Body extends StatelessWidget {
+  const Body({Key? key}) : super(key: key); 
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          ImageAndIcons(size: size),
+          const TitleAndPrice(
+            title: "Angelica",
+            country: "Russia",
+            price: 440,
+          ),
+          const SizedBox(height: kDefaultPadding),
+          Row(
+            children: [
+              SizedBox(
+                width: size.width / 2,
+                height: 84,
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: kPrimaryColor,
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(20),
+                      ),
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: const Text(
+                    "Buy Now",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  onPressed: () {},
+                  child: const Text(
+                    "Description",
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: kPrimaryColor,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
