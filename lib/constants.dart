39ca36ce37c68d768310c953e8d60931634c59d4
buyaramadhan <ytchochote@gmail.com>
2025-05-23 01:33:21 +0700

    tambahkan warna tema dan padding default untuk konsistensi UI

---
+++ lib/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+
+const PPrimaryColor = Color(0xFF0C9869);
+const PTextColor = Color(0xFF3C4046);
+const PBackgroundColor = Color(0xFFF9F8FD);
+const PSecondaryColor = Color(0xFFB0DB9C);
+
+const double pDefaultPadding = 20.0;
