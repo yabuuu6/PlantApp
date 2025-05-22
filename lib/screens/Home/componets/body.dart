196760a1bbd785cbd0b3b30a204e07611143ddd5
buyaramadhan <ytchochote@gmail.com>
2025-05-23 02:31:49 +0700

    layout tampilan halamanhome

---
+++ lib/screens/Home/componets/body.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:plant_app/constants.dart';
+
+import 'featurred_plants.dart';
+import 'header_with_seachbox.dart';
+import 'recomend_plants.dart';
+import 'title_with_more_bbtn.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+           HeaderWithProfileInfo(
+            size: size,
+            name: 'Yabuuu',
+            address: 'jl.wakudara ,yogyakarta',
+          ),
+          TitleWithMoreBtn(title: "Recomended", press: () {}),
+          RecomendsPlants(),
+          TitleWithMoreBtn(title: "Featured Plants", press: () {}),
+          FeaturedPlants(),
+          SizedBox(height: kDefaultPadding),
+        ],
+      ),
+    );
+  }
+}
