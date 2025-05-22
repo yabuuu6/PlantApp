9e9c6903fac49f5eaad3fd472332115abf5b7d44
buyaramadhan <ytchochote@gmail.com>
2025-05-23 02:27:59 +0700

    widget TitleWithMoreBtn dengan tombol

---
+++ lib/screens/Home/componets/title_with_more_bbtn.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:plant_app/constants.dart';
+
+class TitleWithMoreBtn extends StatelessWidget {
+  const TitleWithMoreBtn({
+    Key? key,
+    required this.title,
+    required this.press,
+  }) : super(key: key);
+
+  final String title;
+  final VoidCallback press;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+      child: Row(
+        children: [
+          TitleWithCustomUnderline(text: title),
+          const Spacer(),
+          TextButton(
+            style: TextButton.styleFrom(
+              backgroundColor: kPrimaryColor,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+            ),
+            onPressed: press,
+            child: const Text(
+              "More",
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
