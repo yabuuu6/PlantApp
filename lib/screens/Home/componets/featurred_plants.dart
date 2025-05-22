5d4c224f7cf52a4dc5617a13fb2a33a9943e51b1
buyaramadhan <ytchochote@gmail.com>
2025-05-23 02:03:24 +0700

    menambahkan tampilan kartu dengan gambar interaktif

--- lib/screens/Home/componets/featurred_plants.dart
+++ lib/screens/Home/componets/featurred_plants.dart
@@ -33,4 +33,28 @@ class FeaturePlantCard extends StatelessWidget {
 
   final String image;
   final VoidCallback press;
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return GestureDetector(
+      onTap: press,
+      child: Container(
+        margin: EdgeInsets.only(
+          left: kDefaultPadding,
+          top: kDefaultPadding / 2,
+          bottom: kDefaultPadding / 2,
+        ),
+        width: size.width * 0.8,
+        height: 185,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          image: DecorationImage(
+            fit: BoxFit.cover,
+            image: AssetImage(image),
+          ),
+        ),
+      ),
+    );
+  }
 }
